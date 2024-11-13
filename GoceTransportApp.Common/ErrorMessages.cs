9734ae0f18495ae84756596ce33fccb1376a6d8c
DimitarTashkov <mitkotashkov1979@gmail.com>
2024-11-14 01:12:57 +0200

    Extend Error messages for Route

--- GoceTransportApp.Common/ErrorMessages.cs
+++ GoceTransportApp.Common/ErrorMessages.cs
@@ -8,15 +8,23 @@
 
             public const string StreetEditFailed = "Unexpected error occurred while updating the street! Please contact administrator!";
             public const string StreetDeleteFailed = "Unexpected error occurred while deleting the street! Please contact administrator!";
-
         }
+
         public static class CityMessages
         {
             public const string CityNameRequiredMessage = "Street name is required";
 
             public const string CityEditFailed = "Unexpected error occurred while updating the city! Please contact administrator!";
             public const string CityDeleteFailed = "Unexpected error occurred while deleting the city! Please contact administrator!";
+        }
+        public static class RouteMessages
+        {
+            public const string RouteDistanceRequiredMessage = "Route distance is required";
+            public const string RouteDurationRequiredMessage = "Route duration is required";
 
+
+            public const string RouteEditFailed = "Unexpected error occurred while updating the route! Please contact administrator!";
+            public const string RouteDeleteFailed = "Unexpected error occurred while deleting the route! Please contact administrator!";
         }
     }
 }

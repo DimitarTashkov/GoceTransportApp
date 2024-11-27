b107fd32970e2c49fea09fd92cc7d908a22fcd00
DimitarTashkov <mitkotashkov1979@gmail.com>
2024-11-27 23:41:41 +0200

    Began VehicleService implementation

---
+++ Services/GoceTransportApp.Services.Data/Vehicles/IVehicleService.cs
@@ -0,0 +1,27 @@
+﻿using GoceTransportApp.Web.ViewModels.Drivers;
+using GoceTransportApp.Web.ViewModels.Vehicles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoceTransportApp.Services.Data.Vehicles
+{
+    public interface IVehicleService
+    {
+        Task<IEnumerable<VehicleDataViewModel>> GetAllVehicles();
+
+        Task CreateAsync(VehicleInputModel inputModel);
+
+        Task<EditVehicleInputModel> GetVehicleForEdit(Guid id);
+
+        Task<bool> EditVehicleAsync(EditVehicleInputModel inputModel);
+
+        Task<RemoveVehicleViewModel> GetVehicleForDeletion(Guid id);
+
+        Task<bool> RemoveVehicleAsync(RemoveVehicleViewModel inputModel);
+
+        Task<VehicleDetailsViewModel> VehicleDetails(Guid id);
+    }
+}
---
+++ Services/GoceTransportApp.Services.Data/Vehicles/VehicleService.cs
@@ -0,0 +1,57 @@
+﻿using GoceTransportApp.Data.Common.Repositories;
+using GoceTransportApp.Data.Models;
+using GoceTransportApp.Services.Data.Base;
+using GoceTransportApp.Web.ViewModels.Vehicles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoceTransportApp.Services.Data.Vehicles
+{
+    public class VehicleService : BaseService, IVehicleService
+    {
+        private IDeletableEntityRepository<Vehicle> vehicleRepository;
+
+        public VehicleService(IDeletableEntityRepository<Vehicle> vehicleRepository)
+        {
+            this.vehicleRepository = vehicleRepository;
+        }
+
+        public Task CreateAsync(VehicleInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> EditVehicleAsync(EditVehicleInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<VehicleDataViewModel>> GetAllVehicles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RemoveVehicleViewModel> GetVehicleForDeletion(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<EditVehicleInputModel> GetVehicleForEdit(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveVehicleAsync(RemoveVehicleViewModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<VehicleDetailsViewModel> VehicleDetails(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

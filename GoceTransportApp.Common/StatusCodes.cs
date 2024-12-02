6f60c5d159165deb23abef909c69ce9de39f3d95
DimitarTashkov <mitkotashkov1979@gmail.com>
2024-12-02 13:24:34 +0200

    utilities for our custom pages

---
+++ GoceTransportApp.Common/StatusCodes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoceTransportApp.Common
+{
+    public class StatusCodes
+    {
+        public const int NotFound = 404;
+
+        public const int InternalServerError = 500;
+    }
+}

494eeb77c7dfa062f44521d23ba7e627fd1f1de0
DimitarTashkov <mitkotashkov1979@gmail.com>
2024-12-02 17:29:22 +0200

    admin controller for user management

--- Web/GoceTransportApp.Web/Areas/Administration/Controllers/UserAdministrationController.cs
+++ Web/GoceTransportApp.Web/Areas/Administration/Controllers/UserAdministrationController.cs
@@ -1,15 +1,113 @@
 ﻿namespace GoceTransportApp.Web.Areas.Administration.Controllers
 {
     using GoceTransportApp.Common;
+    using GoceTransportApp.Services.Data.Users;
     using GoceTransportApp.Web.Controllers;
 
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System;
+    using GoceTransportApp.Web.ViewModels.Users;
 
     [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
     [Area("Administration")]
     public class UserAdministrationController : BaseController
     {
+        private readonly IUserService userService;
 
+        public UserAdministrationController(IUserService userService)
+            : base()
+        {
+            this.userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<AllUsersViewModel> allUsers = await this.userService
+                .GetAllUsersAsync();
+
+            return this.View(allUsers);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AssignRole(string userId, string role)
+        {
+            Guid userGuid = Guid.Empty;
+            if (!this.IsGuidValid(userId, ref userGuid))
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            bool userExists = await this.userService
+                .UserExistsByIdAsync(userGuid);
+            if (!userExists)
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            bool assignResult = await this.userService
+                .AssignUserToRoleAsync(userGuid, role);
+            if (!assignResult)
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            return this.RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveRole(string userId, string role)
+        {
+            Guid userGuid = Guid.Empty;
+            if (!this.IsGuidValid(userId, ref userGuid))
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            bool userExists = await this.userService
+                .UserExistsByIdAsync(userGuid);
+            if (!userExists)
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            bool removeResult = await this.userService
+                .RemoveUserRoleAsync(userGuid, role);
+            if (!removeResult)
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            return this.RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteUser(string userId)
+        {
+            Guid userGuid = Guid.Empty;
+            if (!this.IsGuidValid(userId, ref userGuid))
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            bool userExists = await this.userService
+                .UserExistsByIdAsync(userGuid);
+            if (!userExists)
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            bool removeResult = await this.userService
+                .DeleteUserAsync(userGuid);
+            if (!removeResult)
+            {
+                return this.RedirectToAction(nameof(Index));
+            }
+
+            return this.RedirectToAction(nameof(Index));
+        }
     }
 }

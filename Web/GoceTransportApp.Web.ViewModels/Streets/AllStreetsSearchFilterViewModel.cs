013ac3f37d9a4c6ef85118b3d57aee75367d3fc8
DimitarTashkov <mitkotashkov1979@gmail.com>
2024-12-09 22:19:36 +0200

    view model for pagination

---
+++ Web/GoceTransportApp.Web.ViewModels/Streets/AllStreetsSearchFilterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoceTransportApp.Web.ViewModels.Streets
+{
+    public class AllStreetsSearchFilterViewModel
+    {
+        public IEnumerable<StreetDataViewModel> Streets { get; set; }
+
+        public string SearchQuery { get; set; }
+
+        public int? CurrentPage { get; set; } = 1;
+
+        public int? EntitiesPerPage { get; set; } = 5;
+
+        public int? TotalPages { get; set; }
+    }
+}

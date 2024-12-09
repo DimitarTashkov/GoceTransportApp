a5b53c0476b37f1457f3261041677968c087004b
DimitarTashkov <mitkotashkov1979@gmail.com>
2024-12-10 00:38:01 +0200

    Added pagination view models

---
+++ Web/GoceTransportApp.Web.ViewModels/Routes/AllRoutesSearchFilterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using GoceTransportApp.Web.ViewModels.Cities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoceTransportApp.Web.ViewModels.Routes
+{
+    public class AllRoutesSearchFilterViewModel
+    {
+        public IEnumerable<RouteDataViewModel> Routes { get; set; }
+
+        public string SearchQuery { get; set; }
+
+        public string DepartingCityFilter { get; set; }
+
+        public string ArrivingCityFilter { get; set; }
+
+        public int? CurrentPage { get; set; } = 1;
+
+        public int? EntitiesPerPage { get; set; } = 20;
+
+        public int? TotalPages { get; set; }
+    }
+}
---
+++ Web/GoceTransportApp.Web.ViewModels/Vehicles/AllVehiclesSearchFilterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using GoceTransportApp.Web.ViewModels.Streets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoceTransportApp.Web.ViewModels.Vehicles
+{
+    public class AllVehiclesSearchFilterViewModel
+    {
+        public IEnumerable<VehicleDataViewModel> Vehicles { get; set; }
+
+        public string SearchQuery { get; set; }
+
+        public int? CurrentPage { get; set; } = 1;
+
+        public int? EntitiesPerPage { get; set; } = 20;
+
+        public int? TotalPages { get; set; }
+    }
+}

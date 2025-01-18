63bc330e716cd3f1a0250eef25fc0ac192441ca5
DimitarTashkov <mitkotashkov1979@gmail.com>
2025-01-18 18:51:31 +0200

    upload view models for contact us action

---
+++ Web/GoceTransportApp.Web.ViewModels/ContactForms/ContactFormDataViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoceTransportApp.Web.ViewModels.ContactForms
+{
+    public class ContactFormDataViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime DateSubmitted { get; set; }
+    }
+}
---
+++ Web/GoceTransportApp.Web.ViewModels/ContactForms/ContactFormDetailsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoceTransportApp.Web.ViewModels.ContactForms
+{
+    public class ContactFormDetailsViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Title { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime DateSubmitted { get; set; }
+    }
+}

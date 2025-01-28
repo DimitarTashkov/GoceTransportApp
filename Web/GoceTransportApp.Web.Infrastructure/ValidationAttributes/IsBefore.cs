535b862e3e0290012fb84b1a56bf677b80f9744c
DimitarTashkov <mitkotashkov1979@gmail.com>
2025-01-28 17:20:10 +0200

    fixed attribute issues throwing format exception

--- Web/GoceTransportApp.Web.Infrastructure/ValidationAttributes/IsBefore.cs
+++ Web/GoceTransportApp.Web.Infrastructure/ValidationAttributes/IsBefore.cs
@@ -10,17 +10,40 @@ namespace GoceTransportApp.Web.Infrastructure.ValidationAttributes
 {
     public class IsBefore : ValidationAttribute
     {
-        private const string DateTimeFormat = "dd/MM/yyyy";
-        private readonly DateTime date;
+        private readonly string comparisonProperty;
 
-        public IsBefore(string dateInput)
+        public IsBefore(string comparisonProperty)
         {
-            date = DateTime.ParseExact(dateInput, DateTimeFormat, CultureInfo.InvariantCulture);
+            this.comparisonProperty = comparisonProperty;
         }
 
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            if ((DateTime)value >= date) return new ValidationResult(ErrorMessage);
+            var currentValue = value as string;
+
+            var property = validationContext.ObjectType.GetProperty(comparisonProperty);
+            if (property == null)
+            {
+                return new ValidationResult($"Property '{comparisonProperty}' not found.");
+            }
+
+            var comparisonValue = property.GetValue(validationContext.ObjectInstance) as string;
+
+            if (!DateTime.TryParse(currentValue, out var currentDate))
+            {
+                return new ValidationResult("Invalid IssuedDate format.");
+            }
+
+            if (!DateTime.TryParse(comparisonValue, out var comparisonDate))
+            {
+                return new ValidationResult("Invalid ExpiryDate format.");
+            }
+
+            if (currentDate >= comparisonDate)
+            {
+                return new ValidationResult(ErrorMessage ?? $"{validationContext.DisplayName} must be before {comparisonProperty}.");
+            }
+
             return ValidationResult.Success;
         }
     }

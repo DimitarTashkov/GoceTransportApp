7e1ffe106ea2f8adf8be3aa391789f01e8630c1b
DimitarTashkov <mitkotashkov1979@gmail.com>
2025-02-03 18:23:53 +0200

    improved date filtering

---
+++ Web/GoceTransportApp.Web/wwwroot/js/datePlaceholderHandler.js
@@ -0,0 +1,20 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var dateInput = document.getElementById("filterDate");
+
+    if (!dateInput.value) {
+        dateInput.setAttribute("placeholder", "Filter by a date");
+        dateInput.type = "text";
+
+        dateInput.addEventListener("focus", function () {
+            this.type = "date";
+            this.removeAttribute("placeholder");
+        });
+
+        dateInput.addEventListener("blur", function () {
+            if (!this.value) {
+                this.type = "text";
+                this.setAttribute("placeholder", "Filter by a date");
+            }
+        });
+    }
+});

ab4b564bd40405b70c1a6364bb66ab598857918f
DimitarTashkov <mitkotashkov1979@gmail.com>
2025-02-04 16:55:53 +0200

    adjusted the searching and filtering view + added script for better visualization

---
+++ Web/GoceTransportApp.Web/wwwroot/js/timePlaceholderHandler.js
@@ -0,0 +1,20 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var timeInput = document.getElementById("filterTime");
+
+    if (!timeInput.value) {
+        timeInput.setAttribute("placeholder", "Filter by a time");
+        timeInput.type = "text";
+
+        timeInput.addEventListener("focus", function () {
+            this.type = "time";
+            this.removeAttribute("placeholder");
+        });
+
+        timeInput.addEventListener("blur", function () {
+            if (!this.value) {
+                this.type = "text";
+                this.setAttribute("placeholder", "Filter by a time");
+            }
+        });
+    }
+});
